229c7621ea91f7857fa9e0434246c2a3f8c03bea
zhanghaihua <m13581696259@163.com>
2016-09-08 20:34:15 +0800

    as

---
+++ app/src/main/java/com/example/zhanghaihua/apptextsvn/FirstFM.java
@@ -0,0 +1,35 @@
+package com.example.zhanghaihua.apptextsvn;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FirstFM extends Fragment {
+    private View view;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    // TODO: Rename and change types of parameters
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_first_fm, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
